49f0efa64668ab7e183bced5c8b6c7c76d02610d
Will Nelson <willn703@gmail.com>
2021-08-16 16:34:15 -0400

    partway through Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using API.Data;
+using API.Entities;
+using API.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Regisger(NewRegisterRequest newRegisterRequest)
+        {
+
+            if (await UsersExists(newRegisterRequest.UserName)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = newRegisterRequest.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(newRegisterRequest.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users
+                .Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UsersExists(string userName)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == userName.ToLower());
+        }
+    }
+}
---
+++ API/Requests/NewRegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Requests
+{
+    public class NewRegisterRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
