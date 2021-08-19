46d530fd45679d7c60968ec9514e8df90a4c94c8
Will Nelson <willn703@gmail.com>
2021-08-19 10:07:53 -0400

    end of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -12,24 +11,14 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
   ngOnInit() {
     console.log("Init");
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/Users').subscribe(response => {
-      this.users = response;
-      console.log("Users retrieved");
-    }, error => {
-      console.log(error);
-    });
   }
 }
