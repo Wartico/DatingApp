910ad2ae9067d0b43df1606e1ff517251629ffce
Will Nelson <willn703@gmail.com>
2021-09-15 17:12:35 -0400

    preparing to make date input

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,21 +11,48 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, 
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeFrom();
   }
 
+  initializeFrom(){
+    this.registerForm = new FormGroup({
+      gender: new FormControl('male'),
+      username: new FormControl('', Validators.required),
+      knownAs: new FormControl('', Validators.required),
+      dateOfBirth: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, 
+        Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true}
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
+    console.log(this.registerForm.value);
+    /*this.accountService.register(this.model).subscribe(response => {
       console.log(response);
       this.cancel();
     }, error => {
       console.log(error);``
       this.toastr.error(error.error);
-    });
+    });*/
   }
 
   cancel(){
