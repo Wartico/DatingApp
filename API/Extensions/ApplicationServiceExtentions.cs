114aa4d5dd820175700e7cc4968f94ca43b0046f
Will Nelson <willn703@gmail.com>
2021-09-21 16:35:15 -0400

    End of section 14

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -14,6 +14,7 @@ namespace API.Extensions
         {
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IPhotoService, PhotoService>();
---
+++ API/Models/LikeResponse.cs
@@ -0,0 +1,29 @@
+﻿using API.Entities;
+using API.Extensions;
+using System.Linq;
+
+namespace API.Models
+{
+    public class LikeResponse
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public string PhotoUrl { get; set; }
+        public string City { get; set; }
+
+        public static LikeResponse MapFrom(AppUser user)
+        {
+            return new LikeResponse
+            {
+                Username = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            };
+        }
+    }
+}
